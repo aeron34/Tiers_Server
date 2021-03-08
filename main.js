c8e001f0d1992faa80ee05d179b40ee372d4b5fe
aeron34 <sniperud@gmail.com>
2021-03-08 02:12:14 -0500

    Fixing db connection

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const path = require('path');
+const knex = require('knex');
+const pg = require('pg');
+const cors = require('cors');
+
 app = express();
 
 app.use(express.static('images'));
+app.use(cors());
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+var knx = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+});
 
 let characters = {
   "falco": {
@@ -64,14 +81,40 @@ let characters = {
 }
 
 app.get('/data/:name', (req, res) => {
-  console.log(req.params.name);
-  //setTimeout(bloop, 4000, req, res);
-  res.send(characters[req.params.name])
+  knx('characters').where('name', req.params.name)
+  .then(success => {
+    const data = success[0];
+    delete data.name;
+    data.color = data.color.split(',');
+    for(a = 0; a < 3; a++){
+      data.color[a] = parseInt(data.color[a])
+    }
+
+    res.send(data);
+  })
 });
 
 app.get('/data/', (req, res) => {
-  console.log(req.params.name);
-  res.send(characters);
+  knx('characters').select('*')
+  .then(data => {
+    const proper_data = {}
+
+    for(let i = 0; i < data.length; i++) {
+
+      let name = data[i].name;
+      let add_this = {...data[i]};
+      add_this.color = add_this.color.split(',');
+
+      for(a = 0; a < 3; a++){
+        add_this.color[a] = parseInt(add_this.color[a])
+      }
+
+      delete add_this.name
+      proper_data[name] = {...add_this};
+    };
+
+    res.send(proper_data)
+  })
 });
 
-app.listen(process.env.PORT);
+app.listen(5000)//process.env.PORT);
